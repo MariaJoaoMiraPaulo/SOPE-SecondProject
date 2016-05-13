3315378d12d0c2554dc6cee02f906983b66fc8f0
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-05-13 17:49:52 +0100

    Improvments on Gerador.c

--- Gerador.c
+++ Gerador.c
@@ -6,21 +6,77 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
+
+#define FIFO_NAME_LENGTH 10
+int id=0;
+
+typedef enum {NORTH, SOUTH, EAST, WEST} Direction;
+
+typedef struct {
+  Direction direction;
+  int id;
+  float parking_time;
+  char fifo_name[FIFO_NAME_LENGTH] ;
+
+}Vehicles;
+
+Direction get_car_direction(){
+
+  int r = rand() % 4;
+
+  if(r==0)
+    return NORTH;
+  else if(r==1)
+    return SOUTH;
+  else if(r==2)
+    return EAST;
+  else
+    return WEST;
+}
+
+int get_tick_for_next_car(){
+  int r = rand() % 10;
+  int ticks_for_next_car;
+  if(r<5)
+    ticks_for_next_car=0;
+  else if(r<8)
+    ticks_for_next_car=1;
+  else ticks_for_next_car=2;
+
+  return ticks_for_next_car;
+}
+
+int generate_car(){
+//no final da funcao gerar a probabilidade
+  Vehicles vehicle;
+  vehicle.direction=get_car_direction();
+  vehicle.id=id;
+  id++;
+  sprintf(vehicle.fifo_name,"%s%d","fifo",id);
+
+  //Create thread Vehicle
+
+return get_tick_for_next_car();
+
+}
 
 int main(int argc, char* argv[]){
 
+  srand(time(NULL));
   int time_generation=atoi(argv[1]),u_clock=atoi(argv[2]);
+  int ticks_for_next_car=0;
 
   if(argc != 3){
     perror("Invalid number of arguments.\n");
   }
 
   do{
-    printf("Vou adormecer\n");
+    if(ticks_for_next_car == 0)
+      ticks_for_next_car=generate_car();
+    else ticks_for_next_car--;
     //suspends execution of the calling thread for (at least) u_clock*10^3 microseconds.
     usleep(u_clock*pow(10,3));
-    printf("Vou acordar\n");
-    //generateCar();
   time_generation--;
   }while(time_generation>0);
 
